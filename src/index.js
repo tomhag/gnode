1c5d91631f2a5aba8f4123770b12cbc86c702bb4
tomhage <tomhage@protonmail.com>
2019-09-12 12:13:08 +0200

    updated with typeDef Link only

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-//1 Add a new list of dummy data
+//1 Add a new list of dummy data. For now everything is stored in-memory and not in a DB
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
+  description: 'Fullstack tutorial for GraphQL',
 }]
 // 
 let idCount = links.length
@@ -12,7 +12,7 @@ let idCount = links.length
 //2 We have now moved the typeDef constant to an external file (schema.graphql)
 //A link to this file is added to the typeDefs property in the server constant below...
 
-//3 For each root field in any of the root Types (Query, Mutation) we make resolver functions
+//3 For each root field in any of the schema's root Types (Query, Mutation) we make resolver functions
 // These fetches the data requeste from API queries, formated after the typeDefinitions in the
 // schema.graphql file
 
@@ -21,18 +21,12 @@ const resolvers = {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Mutation: {
-    // 2
-    post: (parent, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
-  },
+  // 3
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
 }
 // 3
 const server = new GraphQLServer({
