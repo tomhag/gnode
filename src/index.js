92ff5a1b13f194ea00d1a04b9498bb26a8a0dc9e
tomhage <tomhage@protonmail.com>
2019-09-14 05:58:02 +0200

    added User model in datamodel.prisma, extended GQL-schema and added post-deployment hook in prisma.yml

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { prisma } = require('./generated/prisma-client')
+const { prisma } = require('./generated/prisma-client') // added
 const { GraphQLServer } = require('graphql-yoga')
 
 const resolvers = {
@@ -18,45 +18,9 @@ const resolvers = {
   },
 }
 
-/*
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, args) => links.find(link => link.id === args.id),
-  },
-  //
-  Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    },
-    updateLink: (root, args) => {
-      const index = links.findIndex(link => link.id === args.id);
-      links[index].description = args.description!== undefined ? args.description : links[index].description;
-      links[index].url = args.url !== undefined ? args.url : links[index].url;
-      return links[index];
-    },
-    deleteLink: (root, args) => {
-      for (var i=0; i < links.length-1; i++) {
-        if (links[i].id===args.id) {
-          const removedLink = links[i];
-          links.splice(i,1);
-          return removedLink;
-        }
-      }
-    }, 
-  }
-}
-*/ 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
+  context: { prisma }, // added
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
